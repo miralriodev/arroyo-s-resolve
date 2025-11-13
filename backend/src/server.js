f63b1895a9f8aa35a21d06122137b2994c5f3a4b
miralriodev <miralriodev@gmail.com>
2025-11-13 15:13:05 -0600

    fix(backend): fixing express proxy

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,7 @@ const rateLimit = require('express-rate-limit');
 require('dotenv').config();
 
 const app = express();
+app.set('trust proxy', 1);
 
 // Configuración
 const PORT = process.env.PORT || 3000;
